522b2893defe379a65eb911bb2e07431cb8bf800
Shanmugamsham <abdeshanmugam@gmail.com>
2024-01-30 13:03:58 +0700

    new

---
+++ script.js
@@ -0,0 +1,59 @@
+setTimeout(function time(){
+    result=document.querySelector("h1").innerHTML=(10-1)
+    
+    },(1000))
+    
+    setTimeout(function time(){
+       result=document.querySelector("h1").innerHTML=(10-2)
+       
+       },(2000))
+    
+     setTimeout(function time(){
+          result=document.querySelector("h1").innerHTML=(10-3)
+          
+          },(3000))
+
+          
+          setTimeout(function time(){
+            result=document.querySelector("h1").innerHTML=(10-4)
+            
+            },(4000))
+            
+            setTimeout(function time(){
+               result=document.querySelector("h1").innerHTML=(10-5)
+               
+               },(5000))
+            
+             setTimeout(function time(){
+                  result=document.querySelector("h1").innerHTML=(10-6)
+                  
+                  },(6000))
+
+
+
+                  setTimeout(function time(){
+                    result=document.querySelector("h1").innerHTML=(10-7)
+                    
+                    },(7000))
+                    
+                    setTimeout(function time(){
+                       result=document.querySelector("h1").innerHTML=(10-8)
+                       
+                       },(8000))
+                    
+                     setTimeout(function time(){
+                          result=document.querySelector("h1").innerHTML=(10-9)
+                          
+                          },(9000))
+                      
+                          setTimeout(function time(){
+                            result=document.querySelector("h1").innerHTML=""
+                            
+                            },(10000))
+
+                            setTimeout(function time(){
+                                result=document.querySelector("p").innerHTML="Happy Indian Independence Day"
+                                
+                                },(10000))
+              
+          
